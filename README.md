79719bc31930d426f7ae62bd71b4b0b56b0b7963
i-do-dev <i.do.dev@outlook.com>
2020-06-15 23:13:41 -0700

    plugin structure and setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# Moodle Plugins Curriki Plugin [![Build Status](https://travis-ci.org/adpe/moodle-local_curriki_moodle_plugin.svg?branch=master)](https://travis-ci.org/adpe/moodle-local_curriki_moodle_plugin)
+
+This plugin install a new webservice `local_curriki_moodle_plugin_get_information` which can be use to retrieve information about installed plugins.
+
+## Main features
+
+There are two optional parameters which can be combined to fetch only given data:
+
+| key         | value  | description                             |
+|-------------|--------|-----------------------------------------|
+| type        | string | Only given plugin types will be fetched |
+| contribonly | int    | Only additional plugins will be fetched |
+
+## Installation
+1. Copy this plugin to the `local` directory of your Moodle instance: `git clone https://github.com/adpe/moodle-local_curriki_moodle_plugin.git local/curriki`
+2. Visit the notifications page to complete the install process
+
+For more information, visit [MoodleDocs](https://docs.moodle.org/37/en/Installing_plugins#Installing_manually_at_the_server) for installing contributed modules and plugins.
+
+## Configuration
+Please create a `token` for this new created service `Plugins curriki` using [Site administration](https://FQDN/admin/settings.php?section=webservicetokens). It's important that the user has the capability `moodle/site:config`.
---
+++ db/services.php
@@ -0,0 +1,45 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Web service local plugin template external functions and service definitions.
+ *
+ * @package   local_curriki_moodle_plugin
+ * @copyright 2020 Curriki <info@curriki.org> 
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// We defined the web service functions to install.
+$functions = array(
+    'local_curriki_moodle_plugin_get_information' => array(
+        'classname'   => 'local_curriki_moodle_plugin_external',
+        'methodname'  => 'get_information',
+        'classpath'   => 'local/curriki_moodle_plugin/externallib.php',
+        'description' => 'Curriki Plugin APIs to create resources and publish to LTI.',
+        'type'        => 'read',
+    )
+);
+
+// We define the services to install as pre-build services. A pre-build service is not editable by administrator.
+$services = array(
+    'Curriki Plugin' => array(
+        'functions' => array ('local_curriki_moodle_plugin_get_information'),
+        'restrictedusers' => 0,
+        'enabled' => 1
+    )
+);
