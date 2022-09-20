4ef912f3a71d7cb40897f20d2952754b42f56e6c
Waqar Muneer <63515864+i-do-dev@users.noreply.github.com>
2022-09-20 16:24:40 +0500

    Set moodle compatible version starting from Moodle 3.9.15

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2022012701;
-$plugin->requires = 2020061500;
+$plugin->requires = 2020061515;
 $plugin->component = 'local_curriki_moodle_plugin';
 $plugin->release = 'v1.2';
 $plugin->maturity = MATURITY_STABLE;
